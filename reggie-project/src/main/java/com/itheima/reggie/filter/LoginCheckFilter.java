0e8020a34731c6bccc0628389fd325fb1df4f88c
He zhongyu <1094174805@qq.com>
2023-02-01 01:23:19 +0800

    添加拦截器

---
+++ reggie-project/src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,72 @@
+package com.itheima.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "LoginCheckFilter", urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+
+        String[] urls = {
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+
+        if (check(urls, uri)) {
+            chain.doFilter(req, resp);
+            return;
+        }
+
+        Employee emp = (Employee) req.getSession().getAttribute("employee");
+        if (emp != null) {
+//            说明已经登录过,要放行
+            chain.doFilter(req, resp);
+            return;
+        }
+
+        R<Object> r = R.error("NOTLOGIN");
+        String json = JSON.toJSONString(r);
+//        resp.setContentType("application/json;charset=utf-8");乱码取消注释
+        resp.getWriter().write(json);
+    }
+
+
+    /**
+     * 检查uri是否在urls中
+     *
+     * @param urls
+     * @param uri
+     * @return true:在urls中
+     */
+
+    public boolean check(String[] urls, String uri) {
+        AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+        for (String url : urls) {
+            if (PATH_MATCHER.match(url, uri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
