304583c5dff64fe6f000480d228432a2651fc5a6
He zhongyu <1094174805@qq.com>
2023-01-31 23:17:26 +0800

    实例化启动类

--- reggie-project/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie-project/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -3,9 +3,11 @@ package com.itheima.reggie;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan(basePackages = "com.itheima.reggie.mapper")
 public class ReggieApplication {
     public static void main(String[] args) {
