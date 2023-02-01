3020571f034e9d41c0082ae9d8f1e649fa4db652
He zhongyu <1094174805@qq.com>
2023-02-01 11:15:09 +0800

    新增分页查询功能

--- reggie-project/src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ reggie-project/src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -2,15 +2,14 @@ package com.itheima.reggie.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.IEmployeeService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -74,6 +73,13 @@ public class EmployeeController {
         return R.success("退出成功");
     }
 
+    /**
+     * 保存
+     *
+     * @param employee           员工
+     * @param httpServletRequest http servlet请求
+     * @return {@link R}
+     */
     @PostMapping
     public R save(@RequestBody Employee employee, HttpServletRequest httpServletRequest) {
         employee.setCreateTime(LocalDateTime.now());
@@ -86,6 +92,23 @@ public class EmployeeController {
         employee.setPassword(md5DigestAsHex);
         employeeService.save(employee);
         return R.success("添加成功");
+    }
+
+
+    /**
+     * 分页查询
+     *
+     * @param page     页面
+     * @param pageSize 页面大小
+     * @return {@link R}
+     */
+    @GetMapping("/page")
+    public R page(Integer page, Integer pageSize, String name) {
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);//name like '%name%'
+        Page<Employee> pageInfo = new Page<>(page, pageSize);
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
     }
 }
 
