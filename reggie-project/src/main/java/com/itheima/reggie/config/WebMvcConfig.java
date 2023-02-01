bce077dffa3235c2e84e2d4357722a337c410667
He zhongyu <1094174805@qq.com>
2023-02-01 15:38:01 +0800

    在WebMvcConfig中添加扩展消息转换器

--- reggie-project/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ reggie-project/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,16 @@
 package com.itheima.reggie.config;
 
 
+import com.itheima.reggie.common.JacksonObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
 
 /**
  * web mvc配置
@@ -12,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @date 2023/01/22
  */
 @Configuration
+@Slf4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 添加资源处理程序
@@ -23,5 +31,22 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
 
+    }
+
+    /**
+     * 扩展消息转换器
+     *
+     * @param converters 转换器
+     */
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器");
+//        创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//        设置对象转换器,底层使用jackson将java对象转换成json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+//        将消息转换器添加到转换器列表中
+        converters.add(0, messageConverter);
     }
 }
